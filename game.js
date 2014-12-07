2ba816d0101d34ece71ad475d59ccad8726d8dc3
Jonathan Herman <jdh11235@gmail.com>
2014-12-07 17:53:38 -0600

    file headers

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
+/* Copyright (c) 2014 Jonathan Herman - MIT License */
+/* http://github.com/jdh11235/lightswitch */
 
+var Game = {
+
+}
---
+++ util.js
@@ -0,0 +1,4 @@
+/* Copyright (c) 2014 Jonathan Herman - MIT License */
+/* http://github.com/jdh11235/lightswitch */
+
+var $gameboard = document.getElementById('gameboard');
